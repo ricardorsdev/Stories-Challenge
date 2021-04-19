a0809998dd2e78dfbfb62c2a2f651831e21edd78
Ricardo Sousa <sousa.ricardorodrigues@gmail.com>
2021-04-19 01:18:31 -0300

    creating splash activity, including libraries

--- app/src/main/java/br/com/ricardorsdev/storieschallenge/ui/views/activities/SplashActivity.kt
+++ app/src/main/java/br/com/ricardorsdev/storieschallenge/ui/views/activities/SplashActivity.kt
@@ -1,16 +1,16 @@
 package br.com.ricardorsdev.storieschallenge.ui.views.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 import br.com.ricardorsdev.storieschallenge.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContentView(R.layout.activity_splash)
 	}
-
 }
